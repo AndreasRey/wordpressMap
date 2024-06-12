e9c0a43c8198e36f808e03efe3fec626a449507b
Andreas Rey <andreas07rey@gmail.com>
2024-06-13 01:16:03 +0200

    updated main plugin file

---
+++ webmap/webmap.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: wordpressMap
+Plugin URI: https://github.com/AndreasRey/wordpressMap
+Description: Simple leaflet map integrated into WordPress.
+Version: 1.0
+Author: Andreas Rey
+Author URI: https://www.linkedin.com/in/andreas-rey/
+License: MIT
+*/
+
+function webmap_shortcode() {
+  // Custom HTML content
+  $content = '
+  <div class="custom-html-content">
+      <div class="wordpressmap-content"></div>
+      <h1>Custom HTML Content</h1>
+      <p>This is a custom plugin that outputs HTML content.</p>
+  </div>
+  ';
+  return $content;
+}
+add_shortcode('wordpressmap-html', 'webmap_shortcode');
+
+function webmap_enqueue_scripts() {
+  //wp_enqueue_style('my-custom-plugin-style', plugins_url('/css/style.css', __FILE__));
+  wp_enqueue_script('wordpressmap-script', plugins_url('/script.js', __FILE__), array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'webmap_enqueue_scripts');
