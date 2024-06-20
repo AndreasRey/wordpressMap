a06db00dc671652ddb68018aa1c062466dc8a959
Andreas Rey <andreas07rey@gmail.com>
2024-06-20 23:02:12 +0200

    cleanuo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import './css/style.css';
 import leafletMap from './js/leafletMap';
-//import Icon from './img/icon.png';
 
 function component() {
   let element = document.querySelector('.wordpressmap-content');
